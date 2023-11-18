b033c9623101b527cb0ad8fbcce056f56524180d
Gaspar <gasparbianco98@gmail.com>
2023-11-18 11:18:10 -0300

    First commit

---
+++ src/main/java/com/connectis/connectis/ConnectisApplication.java
@@ -0,0 +1,13 @@
+package com.connectis.connectis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectisApplication.class, args);
+	}
+
+}
