4dfa29b87ab8e09a47e77358b80e3c499163a1ea
Gaspar <gasparbianco98@gmail.com>
2023-11-18 20:25:08 -0300

    get country by id

--- src/test/java/com/connectis/connectis/CountryServicesTests.java
+++ src/test/java/com/connectis/connectis/CountryServicesTests.java
@@ -1,24 +1,32 @@
 package com.connectis.connectis;
 
 import com.connectis.connectis.models.CountryEntity;
-import com.connectis.connectis.repository.ICountryRepository;
 import com.connectis.connectis.services.CountryServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 public class CountryServicesTests {
     @Autowired
     private CountryServices countryServices;
-    @Autowired
-    private ICountryRepository iCountryRepository;
 
     @Test
-    public void addCountry_validName_countryResponse(){
-        CountryEntity addedCountry = countryServices.addCountry("Argentina");
+    public void addCountry_validName_countryEntity(){
+        CountryEntity country = new CountryEntity();
+        country.setName("Argentina");
+        CountryEntity addedCountry = countryServices.addCountry(country);
         assertNotNull(addedCountry.getId());
         assertEquals("Argentina", addedCountry.getName());
+    }
+    @Test
+    public void getCountryById_ValidId_countryEntity(){
+        assertEquals("Argentina", countryServices.getCountryById(1L).getName());
+    }
+    @Test
+    public void getCountryById_invalidId_null(){
+        assertNull(countryServices.getCountryById(5L));
     }
 }
